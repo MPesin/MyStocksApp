516157c41d5fb58bbd6dd04978e7838b4e078803
MPesin <mikelpesin@gmail.com>
2021-06-05 23:38:34 +0300

    added main view - tabs and title

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:my_stocks_app/data/constants.dart';
 import 'package:my_stocks_app/views/main_view.dart';
 
-void main() => runApp(MainView());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('he', ''),
+      ],
+      title: Constants.appName,
+      home: MainView(),
+    );
+  }
+}
--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class MainView extends StatelessWidget {
+class MainView extends StatefulWidget {
+  @override
+  _MainViewState createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  static const totalTabs = 2;
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.trending_up)),
-                Tab(
-                  icon: Icon(Icons.check_box),
-                )
-              ],
-            ),
-            title: ,
+    return DefaultTabController(
+      length: totalTabs,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.trending_up)),
+              Tab(icon: Icon(Icons.check_box))
+            ],
           ),
+          title: Text(AppLocalizations.of(context).appName),
+          centerTitle: true,
         ),
       ),
     );
